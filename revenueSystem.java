13ed2e64791481462111bb5255b44cb4c301fdc4
Haowei Li <91715036+Li-Haowei@users.noreply.github.com>
2021-11-25 17:41:18 -0500

    This is the revenueSystem
    keep tracks of the revenue

---
+++ revenueSystem.java
@@ -0,0 +1,14 @@
+package carRentalSystem;
+import java.util.*;
+/**
+ * 
+ * @author Haowei Li
+ *add records
+ *delete record
+ search record
+ calculate charges
+ update status
+ */
+public class revenueSystem {
+	private String[][] revenueBook;
+}
